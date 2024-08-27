eaeb156b1ccbc7e37b9539f2313a3eebbb074374
le thanh nhan <thanhnhan13082021@gmail.com>
2024-08-27 12:15:06 +0700

    lấy data sản phẩm in lên giao diện

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,8 +1,25 @@
+const Product = require("../../models/product.model");
+
+
+
+
+
+
+
+
+
 // [get] /admin/products
 
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        // deleted: false
+    });
+    console.log(products);
+
 
     res.render("admin/pages/products/index.pug", {
-        pageTitle: "Trang danh sách sản phẩm"
+        pageTitle: "Trang danh sách sản phẩm",
+        // truyen data vao giao dien 
+        products: products
     });
 }
