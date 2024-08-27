3db51b8cb89726ab949de6a59c65d80a6f9150fd
le thanh nhan <thanhnhan13082021@gmail.com>
2024-08-27 07:52:51 +0700

    set up layout and local variable done

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const route = require("./routes/client/index.route")
 const database = require("./config/database")
 database.connect();
 
-
+const systemConfig = require("./config/system")
 const routeAdmin = require("./routes/admin/index.route")
 
 app.set('views','./views');
 app.set('view engine', 'pug');
+// App local variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
+
 
 app.use(express.static("public"));
 routeAdmin(app);
